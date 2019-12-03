90fd2696ebd44703e21a73321351993142577c93
wenye <1583247975@qq.com>
2019-12-03 11:16:28 +0800

    feat(index.ts): 完成代码编写和测试

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
-export function getStr() {
-  return "hello";
+import EventEmitter from "events";
+
+export class AsyncOnceError extends Error {
+  name: string = "AsyncOnceError";
+  code: string = "TIMEOUT";
+  message: string = "超时";
+}
+
+export interface IAsyncOnceOptions {
+  /** 超时时间，默认5秒 */
+  timeout?: number;
+}
+
+export class AsyncOnce {
+  private timeout: number;
+  private event: EventEmitter;
+
+  constructor(opt: IAsyncOnceOptions) {
+    this.timeout = opt.timeout || 5000;
+    this.event = new EventEmitter();
+    this.event.setMaxListeners(Infinity);
+  }
+
+  once<T>(tag: string, fn: () => Promise<T>): Promise<T> {
+    return new Promise((resolve, reject) => {
+      // 添加事件监听器
+      this.event.on(tag, (err: any, ret: T) => {
+        this.event.removeAllListeners(tag); // 清除事件监听防止内存溢出
+        if (err) return reject(err);
+        resolve(ret);
+      });
+      // 首个请求
+      if (this.event.listenerCount(tag) === 1) {
+        // 超时处理
+        setTimeout(() => {
+          if (this.event.eventNames().includes(tag)) {
+            this.event.emit(tag, new AsyncOnceError());
+          }
+        }, this.timeout);
+        // 执行函数
+        fn()
+          .then(ret => {
+            this.event.emit(tag, null, ret);
+          })
+          .catch(err => {
+            this.event.emit(tag, err);
+          });
+      }
+    });
+  }
 }
--- test/test-index.ts
+++ test/test-index.ts
@@ -1,8 +1,114 @@
-import { getStr } from "../src/index";
-import assert from "assert";
+import { AsyncOnce, AsyncOnceError } from "../src/index";
+import { assert } from "chai";
 
-describe("test", function() {
-  it("hello", function() {
-    assert.equal(getStr(), "hello");
+function sleep(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+describe("AsyncOnce", function() {
+  it("获取数据", async function() {
+    const aonce = new AsyncOnce({ timeout: 1000 });
+    const ret1 = await aonce.once("a", async () => {
+      await sleep(50);
+      return "wenye";
+    });
+    assert.strictEqual(ret1, "wenye");
+    const ret2 = await aonce.once("a", async () => {
+      await sleep(50);
+      return "yiye";
+    });
+    assert.strictEqual(ret2, "yiye");
+  });
+
+  it("并发获取数据", async function() {
+    const aonce = new AsyncOnce({ timeout: 1000 });
+    const tag = "test";
+    let counter = 0;
+    async function getData() {
+      counter++;
+      await sleep(50);
+      return "wenye";
+    }
+    const ret = await Promise.all([
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+      aonce.once(tag, getData),
+    ]);
+    assert.strictEqual(counter, 1);
+    assert.deepStrictEqual(ret, [
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+      "wenye",
+    ]);
+  });
+
+  it("并发获取数据超时", async function() {
+    const aonce = new AsyncOnce({ timeout: 50 });
+    const tag = "test";
+    let counter = 0;
+    async function getData() {
+      counter++;
+      await sleep(100);
+      return "wenye";
+    }
+    let isThrowError = false;
+    try {
+      await Promise.all([
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+      ]);
+    } catch (err) {
+      isThrowError = true;
+      assert.instanceOf(err, AsyncOnceError);
+    }
+    assert.strictEqual(isThrowError, true);
+    assert.strictEqual(counter, 1);
+  });
+
+  it("并发获取数据报错", async function() {
+    const aonce = new AsyncOnce({ timeout: 1000 });
+    const tag = "test";
+    let counter = 0;
+    async function getData() {
+      counter++;
+      await sleep(50);
+      throw new Error("test err");
+    }
+    let isThrowError = false;
+    try {
+      await Promise.all([
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+        aonce.once(tag, getData),
+      ]);
+    } catch (err) {
+      isThrowError = true;
+      assert.strictEqual(err.message, "test err");
+    }
+    assert.strictEqual(isThrowError, true);
+    assert.strictEqual(counter, 1);
   });
 });
